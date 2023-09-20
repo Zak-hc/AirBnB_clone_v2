73c894b75aec137145cf9200d47b5ad311a9d634
Zack-hc <zhaci001@gmail.com>
2023-09-20 06:58:17 -0700

    printing by us

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- script sql
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost'
