4436b78a3b78653a0e9060b089c9b37a685020df
Zack-hc <zhaci001@gmail.com>
2023-09-21 14:07:06 -0700

    printing by us

--- models/state.py
+++ models/state.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from slqalchemy import Column, String, Integer, Datetime, Foreignkey
+import sqlalchemy
+from sqlalchemy.orm import relationship
+from models.city import city
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable = False)
+    
+    cities = relationship("City", backref = "state", cascade = "delete")
+
