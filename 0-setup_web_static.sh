1ed294f334ebe0f669497331ff07d502135198ab
Zack-hc <zhaci001@gmail.com>
2023-10-09 14:24:31 -0700

    task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#comment
+apt-get update
+apt-get install nginx -y
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo “ya saaaalam” >  /data/web_static/releases/test/index.html
+ln -sf   /data/web_static/releases/test/  /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+v="location /hbnb_static { alias /data/web_static/current/; }"
+n="root /var/www/html;"
+sed -i "\%^\t$n%a\ \t$v" /etc/nginx/sites-available/default
+nginx -t
+service nginx restart
