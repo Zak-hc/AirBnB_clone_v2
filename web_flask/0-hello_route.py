69cc4c8c61807f4bf81227124c31856fa9003223
Zack-hc <zhaci001@gmail.com>
2023-10-22 16:26:59 -0700

    task 0ve

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-#flasck
+'''flasck initialisation'''
 from flask import *
+
+
 app = Flask(__name__)
+
+
 @app.route("/", strict_slashes=False)
 def make():
     '''lll'''
     return "Hello HBNB!"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
