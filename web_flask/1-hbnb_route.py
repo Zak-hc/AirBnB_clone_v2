8d9136d514f1246b4f3cf314e763c3c8a162de22
Zack-hc <zhaci001@gmail.com>
2023-10-22 16:30:43 -0700

    task

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''flasck initialisation'''
+from flask import *
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def make():
+    '''lll'''
+    return "Hello HBNB!"
+@app.route("/hbnb", strict_slashes=False)
+def mako():
+    '''lll'''
+    return "HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
